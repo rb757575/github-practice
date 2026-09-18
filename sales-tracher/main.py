d72893f4bfb50aeea5995392379e41c07529fc66
Soheila Bozorgi <bozorgisoheila5@gmail.com>
2026-09-18 20:32:48 +0200

    update main.py with total revenue calculation

--- sales-tracher/main.py
+++ sales-tracher/main.py
@@ -3,4 +3,6 @@ import pandas as pd
 df = pd.read_csv('sales.csv')
 df['total_sales'] = df['price'] * df['quantity']
 
+print("--- Sales Summary ---")
 print(df)
+print("\nTotal Revenue:", df['total_sales'].sum())
